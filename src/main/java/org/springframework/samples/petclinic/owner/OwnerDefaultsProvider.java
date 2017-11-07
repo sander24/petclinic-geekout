4338980efba4384bb54ad1fc7500089150c76421
Sander Sonajalg <sander.sonajalg@gmail.com>
2017-11-07 18:51:44 +0200

    initial improt

---
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerDefaultsProvider.java
@@ -0,0 +1,13 @@
+//package org.springframework.samples.petclinic.owner;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class OwnerDefaultsProvider {
+//  
+//  public void setDefaults(Owner owner) {
+//    if (owner.getCity() == null) {
+//      owner.setCity("Valga");
+//    }
+//  }
+//}
